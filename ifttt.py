ff569924228e7ce12b30626aecaac5bed5b2bf74
Wesley Muthemba <chegeamunga@hotmail.co.uk>
2021-01-25 20:12:33 -0800

    IFTTT connection done

---
+++ ifttt.py
@@ -0,0 +1,18 @@
+import urllib.request
+import time
+import sentiment
+
+compound = sentiment.vs['compound']
+
+while True:
+    if compound <= -0.05: # negative sentiment
+        color = 'red'
+    elif compound >= 0.05: # positive sentiment
+        color = 'green'
+    else: # neutral sentiment
+        color = 'yellow'
+
+    # IFTTT webhook
+    url = 'https://maker.ifttt.com/trigger/{}/with/key/NzX9u8NuVPaFWZyqQRhlv'.format(color)
+    urllib.request.urlopen(url)
+    time.sleep(60) # timer for 60 secs
