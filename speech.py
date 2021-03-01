41d8a8c41b70fb5cc8b51d121dac76598047fc2d
Wesley Muthemba <chegeamunga@hotmail.co.uk>
2021-03-01 00:33:57 -0800

    Speech file re-done for continuous listening

--- speech.py
+++ speech.py
@@ -1,24 +1,30 @@
-#import library
 import speech_recognition as sr
 
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
+m = sr.Microphone()
 
 # A list
 sentences = []
 
-# Reading Microphone as source
-# listening the speech and store in audio_text variable
-with sr.Microphone() as source:
-    print("Talk")
-    audio_text = r.listen(source)
-    print("Time over, thanks")
-
-# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling    
-    try:
-        # using google speech recognition
-        capture = r.recognize_google(audio_text)
-        sentences.append(capture)
-        print("Text:" + capture)
-    except:
-        print("Sorry, I did not get that")  
+try:
+    print("A moment of silence, please...")
+    with m as source: r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
+    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    
+    while True:
+        print("Say something!")
+        with m as source: audio = r.listen(source, phrase_time_limit=10) # wait for 10 secs to detect speech
+        print("Got it! Now to recognize it...")
+        
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio)
+            sentences.append(value)
+            print("You said {}".format(value))
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+except KeyboardInterrupt:
+    pass
